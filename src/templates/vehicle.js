b6d86b6cb35d1e93761fc1aa150645eb4eb37210
ivowomon <carlos.art.solis.s@gmail.com>
2020-07-19 12:12:16 -0600

    add image slider to vehicle details

--- src/templates/vehicle.js
+++ src/templates/vehicle.js
@@ -1,9 +1,28 @@
 import React, {useState} from "react"
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 import HomeLayout from "../layouts/homeLayout"
 import VehicleCard from "../components/vehicleCard/vehicleCard"
 
+const TEMP_IMAGES = [
+  '/harley.jpg',
+  '/red.jpg',
+  '/red-white.jpg',
+  '/trike.jpg',
+  '/vintage.jpg',
+  '/white-blue.jpg',
+]
+const CustomSlider = () => (
+  <Carousel showArrows={true} >
 
+    {TEMP_IMAGES.map((imagePath) => (
+      <div key={`${imagePath}-carousel`}>
+          <img src={imagePath} />
+      </div>
+    ))}
+  </Carousel>
+);
 
 const MainCTA = ({ ownerContact }) => {
   const [showContact, setContact] = useState(false)
@@ -20,6 +39,7 @@ export default function Home(props) {
   
   return (
     <HomeLayout>
+      <CustomSlider />
       <VehicleCard node={postData} />
       <MainCTA ownerContact={'+(##) ####-####'}/>
     </HomeLayout>
